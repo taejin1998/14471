c5c72663a8e922f8b8edbc1d2e6f66277948a2e2
taejin1998 <taejin1998@gmail.com>
2026-02-09 17:42:53 +0900

    6강 4단계 구현

--- IdeaProjects/14471/src/main/java/com/back/Main.java
+++ IdeaProjects/14471/src/main/java/com/back/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int id = 1;
 
         System.out.println("== 명언 앱 ==");
 
@@ -18,7 +19,8 @@ public class Main {
                 sc.nextLine();
                 System.out.print("작가 : ");
                 sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(id + "번 명언이 등록되었습니다.");
+                id++;
             } else if (cmd.equals("종료")) {
                 break;
             }
