e961cdfea4d644414d0e2e8cbd9b2d3269a6997e
taejin1998 <taejin1998@gmail.com>
2026-02-09 15:03:25 +0900

    3강 1단계 구현

--- IdeaProjects/14471/src/main/java/com/back/Main.java
+++ IdeaProjects/14471/src/main/java/com/back/Main.java
@@ -1,9 +1,16 @@
 package com.back;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
+        System.out.print("명령) ");
+        sc.nextLine();
+
+        System.out.println("종료");
     }
 }
